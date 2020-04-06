39026fc55765d29130d05b04fde928ff0c3140a8
Bibarius <verastuk.vasil@yandex.ru>
2020-04-06 18:33:06 +0700

    optimizations

--- scrapper.py
+++ scrapper.py
@@ -1,10 +1,11 @@
 from pytube import YouTube
+from json import JSONEncoder
 
 
 def get_download_urls(url):
     links = []
     streams = YouTube(url).streams.filter(progressive=True)
     for stream in streams:
-        links.append(stream.url)
+        links.append({stream.resolution: stream.url})
 
     return links
